5e044f3f9fb03f7ea32ff3557db59d60a33d91e5
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2024-01-08 02:14:23 +0900

    :sparkles: Feat: create project

---
+++ src/main/java/com/aesp/AespApplication.java
@@ -0,0 +1,13 @@
+package com.aesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AespApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AespApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aesp/AespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AespApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
