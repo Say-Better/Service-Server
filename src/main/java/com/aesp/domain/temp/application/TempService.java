4006b5c1d9dc9a7c2f3030e84d431701521715ac
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2024-01-13 15:41:07 +0900

    :sparkles: Feat: Success Method 작성
    <body>
    - 성공하는 메서드 작성 및 반환값 테스트
    
    <footer>
    - 관련: #5

--- src/main/java/com/aesp/domain/temp/application/TempService.java
+++ src/main/java/com/aesp/domain/temp/application/TempService.java
@@ -1,4 +1,13 @@
 package com.aesp.domain.temp.application;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class TempService {
+	
 }
--- src/main/java/com/aesp/domain/temp/ui/TempController.java
+++ src/main/java/com/aesp/domain/temp/ui/TempController.java
@@ -1,4 +1,26 @@
 package com.aesp.domain.temp.ui;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aesp.domain.temp.application.TempService;
+import com.aesp.global.common.response.ResponseDto;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/temp")
 public class TempController {
+
+	private final TempService tempService;
+
+	@GetMapping("")
+	public ResponseDto<String> test() {
+		return ResponseDto.onSuccess("Service is running");
+	}
+
 }
