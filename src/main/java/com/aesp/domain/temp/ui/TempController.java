95206fe56129244f3109fb91f3af993b8087423f
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2024-01-27 21:18:27 +0900

    :test_tube: Test: Swagger 설정 작동 test
    <body>
    - test용 api에 swagger 적용
    
    <footer>
    - 관련: #9

--- src/main/java/com/aesp/domain/temp/ui/TempController.java
+++ src/main/java/com/aesp/domain/temp/ui/TempController.java
@@ -10,9 +10,13 @@ import com.aesp.domain.temp.converter.TempResponseConverter;
 import com.aesp.domain.temp.dto.TempResponse;
 import com.aesp.global.common.response.ResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "Temp", description = "Swagger Test API")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -21,11 +25,14 @@ public class TempController {
 
 	private final TempService tempService;
 
+	@Operation(summary = "Running Test API", description = "서비스 동작 확인을 위한 test api")
 	@GetMapping("")
 	public ResponseDto<String> test() {
 		return ResponseDto.onSuccess("Service is running");
 	}
 
+	@Operation(summary = "Exception Test", description = "Exception 발생 확인을 위한 test api")
+	@Parameter(name = "flag", description = "1을 입력하면 예외가 발생됩니다.")
 	@GetMapping("/exception")
 	public ResponseDto<TempResponse.ExceptionTestDto> exception(@RequestParam("flag") int flag) {
 		String message = tempService.exception(flag);
