5bbb1a3fc09baa5198aac9bb8766471a30f4fc6e
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2024-01-27 22:23:24 +0900

    :art: Style: SwaggerConfig 위치 변경
    <body>
    - docs 디렉토리 추가
    
    <footer>
    - 관련: #10

--- src/main/java/com/aesp/global/config/docs/SwaggerConfig.java
+++ src/main/java/com/aesp/global/config/docs/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.aesp.global.config;
+package com.aesp.global.config.docs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
