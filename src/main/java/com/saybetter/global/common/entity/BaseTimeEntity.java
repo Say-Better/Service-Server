4cfa62ff6299bc8f3dd24cd8376d231dd9f7ea5a
SunKyu Choi <zjvl324@gmail.com>
2024-02-16 00:44:04 +0900

    :sparkles: Feat: BaseTimeEntity 생성
    <footer>
    - 관련: #12

---
+++ src/main/java/com/saybetter/global/common/entity/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package com.saybetter.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners({AuditingEntityListener.class})
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
