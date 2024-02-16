d132354ef357cf256844fda69b7f7c8352cd697a
SunKyu Choi <zjvl324@gmail.com>
2024-02-16 11:18:01 +0900

    :sparkles: Feat: Table 생성 에러 해결
    <footer>
    - 관련: #12

--- src/main/java/com/saybetter/domain/solution/domain/SolutionSymbol.java
+++ src/main/java/com/saybetter/domain/solution/domain/SolutionSymbol.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -26,11 +27,11 @@ public class SolutionSymbol {
 	private Long ssId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@Column(name = "solution_id", nullable = false)
+	@JoinColumn(name = "solution_id", nullable = false)
 	private Solution solution;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@Column(name = "symbol_id", nullable = false)
+	@JoinColumn(name = "symbol_id", nullable = false)
 	private Symbol symbol;
-	
+
 }
