aed657d8faa97f7c06d0d427ea31bbce9e2a9b4a
SunKyu Choi <zjvl324@gmail.com>
2024-02-16 19:28:30 +0900

    :recycle: Refactor: member domain service 구조 변경
    <footer>
    - 관련: #12

--- src/main/java/com/saybetter/domain/member/application/impl/ConnectService.java
+++ src/main/java/com/saybetter/domain/member/application/impl/ConnectService.java
@@ -2,12 +2,15 @@ package com.saybetter.domain.member.application.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.saybetter.domain.member.dao.repository.ConnectReadRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class MemberCommandService {
+public class ConnectService {
 
+	private final ConnectReadRepository connectReadRepository;
 }
--- src/main/java/com/saybetter/domain/member/application/impl/MemberService.java
+++ src/main/java/com/saybetter/domain/member/application/impl/MemberService.java
@@ -3,6 +3,7 @@ package com.saybetter.domain.member.application.impl;
 import org.springframework.stereotype.Service;
 
 import com.saybetter.domain.member.dao.repository.MemberReadRepository;
+import com.saybetter.domain.member.dao.repository.MemberWriteRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class MemberQueryService {
+public class MemberService {
 
 	private final MemberReadRepository memberReadRepository;
+	private final MemberWriteRepository memberWriteRepository;
+
 }
