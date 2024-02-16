92945bbb777fa276fddc616595d0e45e4910c06f
SunKyu Choi <zjvl324@gmail.com>
2024-02-16 11:18:30 +0900

    :sparkles: Feat: Constant 데이터 정의
    <footer>
    - 관련: #12

--- src/main/java/com/saybetter/domain/symbol/domain/constant/SymbolType.java
+++ src/main/java/com/saybetter/domain/symbol/domain/constant/SymbolType.java
@@ -1,4 +1,13 @@
 package com.saybetter.domain.symbol.domain.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SymbolType {
+	BASIC("기본"),
+	ADVANCED("고급");
+
+	private final String description;
 }
