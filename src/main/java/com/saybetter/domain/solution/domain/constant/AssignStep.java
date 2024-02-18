ecd8b0aa45aced96d50a0a45cf1fad8c10bd5df5
SunKyu Choi <zjvl324@gmail.com>
2024-02-19 05:11:22 +0900

    :sparkles: Feat: Eum 오류 픽스
    <footer>
    - 관련: #14

--- src/main/java/com/saybetter/domain/solution/domain/constant/AssignStep.java
+++ src/main/java/com/saybetter/domain/solution/domain/constant/AssignStep.java
@@ -6,4 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AssignStep {
+	BASELINE("기초선"),
+	INTERVENTION("중재"),
+	MAINTENANCE("유지");
+
+	private final String description;
 }
