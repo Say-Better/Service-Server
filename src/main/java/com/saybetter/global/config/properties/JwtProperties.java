ab35f48f662935501fa72a50f7219e4dd6beafa8
SunKyu Choi <zjvl324@gmail.com>
2024-02-22 15:43:07 +0900

    :sparkles: Feat: jwt properties 설정 변경
    <footer>
    - 관련: #13

--- src/main/java/com/saybetter/global/config/properties/JwtProperties.java
+++ src/main/java/com/saybetter/global/config/properties/JwtProperties.java
@@ -3,20 +3,15 @@ package com.saybetter.global.config.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-@ConfigurationProperties("jwt")
+@ConfigurationProperties(prefix = "jwt")
 @ConfigurationPropertiesBinding
-public class JwtProperties {
-
-	private String bearer;
-	private String secret;
-	private String accessHeader;
-	private Long accessExpiration;
-	private Long refreshExpiration;
-	private String refreshHeader;
+public record JwtProperties(
+		String bearer,
+		String secret,
+		String accessHeader,
+		Long accessExpiration,
+		Long refreshExpiration,
+		String refreshHeader
+) {
 
 }
