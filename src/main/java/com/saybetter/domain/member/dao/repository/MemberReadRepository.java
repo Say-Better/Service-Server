f2b3b5b3723d12a596ee284fb853f378192b7762
SunKyu Choi <zjvl324@gmail.com>
2024-02-22 20:28:27 +0900

    :sparkles: Feat: Member 조회 기능 추가
    <footer>
    - 관련: #13

--- src/main/java/com/saybetter/domain/member/dao/repository/MemberReadRepository.java
+++ src/main/java/com/saybetter/domain/member/dao/repository/MemberReadRepository.java
@@ -1,8 +1,15 @@
 package com.saybetter.domain.member.dao.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.saybetter.domain.member.domain.Member;
+import com.saybetter.global.common.constant.Provider;
 
 public interface MemberReadRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByLoginId(String loginId);
+
+	Optional<Member> findByProviderAndLoginId(Provider provider, String loginId);
 }
