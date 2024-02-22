e2055c62b311f765a3daf05746166fdd00d2fd3c
SunKyu Choi <zjvl324@gmail.com>
2024-02-22 20:25:26 +0900

    :sparkles: Feat: Constant 내용 수정
    <footer>
    - 관련: #13

--- src/main/java/com/saybetter/global/common/constant/RoleType.java
+++ src/main/java/com/saybetter/global/common/constant/RoleType.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum RoleType {
 	EDUCATOR("교육자"),
 	LEARNER("학습자"),
-	ADMIN("관리자");
+	ADMIN("관리자"),
+	NONE("초기 상태");
 
 	private final String description;
 }
