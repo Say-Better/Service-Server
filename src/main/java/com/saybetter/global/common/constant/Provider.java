8553a029263fe992c5e35b85bc2cf8d368465f50
SunKyu Choi <zjvl324@gmail.com>
2024-02-22 20:25:26 +0900

    :sparkles: Feat: Constant 내용 수정
    <footer>
    - 관련: #13

--- src/main/java/com/saybetter/global/common/constant/Provider.java
+++ src/main/java/com/saybetter/global/common/constant/Provider.java
@@ -1,5 +1,15 @@
 package com.saybetter.global.common.constant;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.saybetter.global.auth.exception.AuthException;
+import com.saybetter.global.common.code.status.ErrorStatus;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +21,21 @@ public enum Provider {
 	KAKAO("kakao"),
 	FACEBOOK("facebook");
 
-	private final String providerName;
+	private final String description;
+
+	private static final Map<String, Provider> descriptions =
+			Collections.unmodifiableMap(
+					Stream.of(values())
+							.collect(
+									Collectors.toMap(
+											Provider::getDescription,
+											Function.identity()
+									)
+							));
+
+	public static Provider find(String description) {
+		return Optional
+				.ofNullable(descriptions.get(description))
+				.orElseThrow(() -> new AuthException(ErrorStatus.INTERNAL_SERVER_ERROR));
+	}
 }
