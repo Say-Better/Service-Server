43c2585d91b9b48b27ee0b50dc4b3919b48641b4
SunKyu Choi <zjvl324@gmail.com>
2024-02-23 18:52:14 +0900

    :sparkles: Feat: 인증코드 인증 및 연동 기능 구현
    <footer>
    - 관련: #22

--- src/main/java/com/saybetter/domain/member/application/MemberFacade.java
+++ src/main/java/com/saybetter/domain/member/application/MemberFacade.java
@@ -2,8 +2,11 @@ package com.saybetter.domain.member.application;
 
 import org.springframework.stereotype.Component;
 
+import com.saybetter.domain.member.application.impl.ConnectService;
 import com.saybetter.domain.member.application.impl.MemberService;
 import com.saybetter.domain.member.domain.Member;
+import com.saybetter.domain.member.exception.MemberException;
+import com.saybetter.global.common.code.status.ErrorStatus;
 import com.saybetter.global.utils.CodeUtil;
 import com.saybetter.global.utils.RedisUtil;
 
@@ -18,15 +21,26 @@ public class MemberFacade {
 	private final CodeUtil codeUtil;
 	private final RedisUtil redisUtil;
 	private final MemberService memberService;
+	private final ConnectService connectService;
 
-	public void createConnectCode() {
+	public String createConnectCode() {
 		Member member = memberService.getCurrentMember();
 		String code = codeUtil.createConnectCode();
 		redisUtil.setConnectCode(code, member.getEmail());
+
+		return code;
 	}
 
-	public String getConnectCode() {
-		Member member = memberService.getCurrentMember();
-		return redisUtil.getData(member.getEmail());
+	public void connect(String code) {
+		String email = redisUtil.getData(code);
+
+		if (email == null)
+			throw new MemberException(ErrorStatus.CONNECT_CODE_NOT_VALID);
+
+		redisUtil.deleteData(code);
+
+		Member educator = memberService.getCurrentMember();
+		Member learner = memberService.getMember(email);
+		connectService.connect(educator, learner);
 	}
 }
