9281f52411fe6d5fabb72e9f442791b27913e0c6
SunKyu Choi <zjvl324@gmail.com>
2024-02-23 18:33:48 +0900

    :sparkles: Feat: 인증코드 발급 기능 구현
    <footer>
    - 관련: #22

--- src/main/java/com/saybetter/domain/member/application/AuthFacade.java
+++ src/main/java/com/saybetter/domain/member/application/AuthFacade.java
@@ -3,8 +3,8 @@ package com.saybetter.domain.member.application;
 import org.springframework.stereotype.Component;
 
 import com.saybetter.domain.member.application.impl.MemberService;
+import com.saybetter.domain.member.domain.Member;
 import com.saybetter.global.common.constant.RoleType;
-import com.saybetter.global.utils.SecurityUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,11 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AuthFacade {
 
-	private final SecurityUtil securityUtil;
 	private final MemberService memberService;
 
 	public void assignUserRole(RoleType role) {
-		String userEmail = securityUtil.getCurrentUserEmail();
-		memberService.assignUserRole(userEmail, role);
+		Member member = memberService.getCurrentMember();
+		memberService.assignUserRole(member.getEmail(), role);
 	}
 }
--- src/main/java/com/saybetter/domain/member/application/impl/MemberService.java
+++ src/main/java/com/saybetter/domain/member/application/impl/MemberService.java
@@ -8,6 +8,7 @@ import com.saybetter.domain.member.domain.Member;
 import com.saybetter.domain.member.exception.MemberException;
 import com.saybetter.global.common.code.status.ErrorStatus;
 import com.saybetter.global.common.constant.RoleType;
+import com.saybetter.global.utils.SecurityUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +18,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class MemberService {
 
+	private final SecurityUtil securityUtil;
+
 	private final MemberReadRepository memberReadRepository;
 	private final MemberWriteRepository memberWriteRepository;
+
+	public Member getCurrentMember() {
+		String email = securityUtil.getCurrentUserEmail();
+		return getMember(email);
+	}
 
 	public Member getMember(String email) {
 		return memberReadRepository.findByEmail(email)
