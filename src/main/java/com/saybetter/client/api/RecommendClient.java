f9e39899f43cb1977182a4cd234610fb52497fff
SunKyu Choi <zjvl324@gmail.com>
2024-02-25 17:38:24 +0900

    :sparkles: Feat: Symbol Recommend api 호출 기능 추가
    <footer>
    - 관련: #24

--- src/main/java/com/saybetter/client/api/RecommendClient.java
+++ src/main/java/com/saybetter/client/api/RecommendClient.java
@@ -1,6 +1,11 @@
 package com.saybetter.client.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.saybetter.client.api.dto.RecommendRequest;
+import com.saybetter.client.api.dto.RecommendResult;
+import com.saybetter.client.converter.RecommendResultConverter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +15,16 @@ public class RecommendClient {
 
 	private final RecommendApi recommendApi;
 
+	@Value("${recommend.api.identity-token}")
+	private String identityToken;
+
+	public RecommendResult.SymbolRecommend recommend(String requestParameter) {
+		RecommendRequest.SymbolRecommend request = RecommendRequest.SymbolRecommend.builder()
+				.name(requestParameter)
+				.build();
+
+		return RecommendResultConverter.toSymbolRecommend(
+				recommendApi.recommend(identityToken, request)
+		);
+	}
 }
--- src/main/java/com/saybetter/client/api/dto/RecommendRequest.java
+++ src/main/java/com/saybetter/client/api/dto/RecommendRequest.java
@@ -1,4 +1,17 @@
 package com.saybetter.client.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
 public class RecommendRequest {
+
+	private RecommendRequest() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	@Builder
+	@AllArgsConstructor
+	public static class SymbolRecommend {
+		private String name;
+	}
 }
