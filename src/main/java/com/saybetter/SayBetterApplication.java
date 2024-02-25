f5c9c4336027cb4f05047764b53a067e9b176f0f
SunKyu Choi <zjvl324@gmail.com>
2024-02-25 17:04:55 +0900

    :wrench: Chore: spring boot config 설정 추가 및 변경
    <body>
    - properties
      - properties 관련 config 위치 변경
    - web
      - client 관련 config 설정 추가
    
    <footer>
    - 관련: #24

--- src/main/java/com/saybetter/SayBetterApplication.java
+++ src/main/java/com/saybetter/SayBetterApplication.java
@@ -2,12 +2,10 @@ package com.saybetter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackages = "com.saybetter.global.config")
 public class SayBetterApplication {
 
 	public static void main(String[] args) {
