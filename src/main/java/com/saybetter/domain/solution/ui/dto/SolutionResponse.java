3269d30fa87441dd54c919865963a83e0444f284
SunKyu Choi <zjvl324@gmail.com>
2024-02-25 17:54:58 +0900

    :sparkles: Feat: Symbol 추천 api 구현
    <footer>
    - 관련: #24

--- src/main/java/com/saybetter/domain/solution/ui/dto/SolutionResponse.java
+++ src/main/java/com/saybetter/domain/solution/ui/dto/SolutionResponse.java
@@ -1,4 +1,24 @@
 package com.saybetter.domain.solution.ui.dto;
 
+import java.util.List;
+
+import lombok.Builder;
+
 public class SolutionResponse {
+
+	private SolutionResponse() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	@Builder
+	public static class SymbolRecommend {
+		private String name;
+		private List<SymbolInfo> symbols;
+	}
+
+	@Builder
+	public static class SymbolInfo {
+		private String description;
+		private String imageUrl;
+	}
 }
--- src/main/java/com/saybetter/domain/symbol/dao/repository/SymbolReadRepository.java
+++ src/main/java/com/saybetter/domain/symbol/dao/repository/SymbolReadRepository.java
@@ -1,8 +1,12 @@
 package com.saybetter.domain.symbol.dao.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.saybetter.domain.symbol.domain.Symbol;
 
 public interface SymbolReadRepository extends JpaRepository<Symbol, Long> {
+	List<Symbol> findByTitleIn(List<String> symbols);
+
 }
