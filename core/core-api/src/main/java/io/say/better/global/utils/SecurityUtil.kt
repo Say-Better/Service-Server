7f04d047e2745ba24c21b8b335869486463ce231
luke0408 <zjvl324@gmail.com>
2024-03-22 01:25:54 +0900

    :recycle: Refactor: utils 패키지 리팩터링
    <footer>
    - 관련: #33

--- core/core-api/src/main/java/io/say/better/global/utils/SecurityUtil.kt
+++ core/core-api/src/main/java/io/say/better/global/utils/SecurityUtil.kt
@@ -1,34 +1,31 @@
-package io.say.better.global.utils;
+package io.say.better.global.utils
 
-import java.util.Collection;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.NoArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
 
 @Slf4j
 @Component
 @NoArgsConstructor
-public class SecurityUtil {
+class SecurityUtil {
+    fun clearSecurityContext() {
+        SecurityContextHolder.clearContext()
+    }
 
-	public void clearSecurityContext() {
-		SecurityContextHolder.clearContext();
-	}
+    val currentUserEmail: String
+        get() {
+            val authentication =
+                    SecurityContextHolder.getContext().authentication as UsernamePasswordAuthenticationToken
+            return authentication.name
+        }
 
-	public String getCurrentUserEmail() {
-		UsernamePasswordAuthenticationToken authentication =
-				(UsernamePasswordAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
-		return authentication.getName();
-	}
-
-	public Collection<GrantedAuthority> getUserAuthorities() {
-		UsernamePasswordAuthenticationToken authentication =
-				(UsernamePasswordAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
-		return authentication.getAuthorities();
-	}
-
+    val userAuthorities: Collection<GrantedAuthority>
+        get() {
+            val authentication =
+                    SecurityContextHolder.getContext().authentication as UsernamePasswordAuthenticationToken
+            return authentication.authorities
+        }
 }
