bf7978f923acf687d24ab75c5f44014736cee34b
luke0408 <zjvl324@gmail.com>
2024-03-22 09:58:11 +0900

    :recycle: Refactor: code 불변 인수 getter 적용
    <body>
    - public val 사용으로 불변인수 적용
    - fun 대신 get()으로 변경
    
    <footer>
    - 관련: #33

--- core/core-api/src/main/java/io/say/better/global/common/code/BaseCode.kt
+++ core/core-api/src/main/java/io/say/better/global/common/code/BaseCode.kt
@@ -4,7 +4,8 @@ import io.say.better.global.common.code.status.SuccessStatus
 import io.say.better.global.common.response.ResponseDto.ReasonDto
 
 interface BaseCode {
-    fun reason(status: SuccessStatus): ReasonDto
 
-    fun reasonHttpStatus(status: SuccessStatus): ReasonDto
+    val reason: ReasonDto
+
+    val reasonHttpStatus: ReasonDto
 }
--- core/core-api/src/main/java/io/say/better/global/common/code/BaseErrorCode.kt
+++ core/core-api/src/main/java/io/say/better/global/common/code/BaseErrorCode.kt
@@ -1,10 +1,10 @@
 package io.say.better.global.common.code
 
-import io.say.better.global.common.code.status.ErrorStatus
 import io.say.better.global.common.response.ResponseDto.ErrorReasonDto
 
 interface BaseErrorCode {
-    fun reason(status: ErrorStatus): ErrorReasonDto
 
-    fun reasonHttpStatus(status: ErrorStatus): ErrorReasonDto
+    val reason: ErrorReasonDto
+
+    val reasonHttpStatus: ErrorReasonDto
 }
