5ef799a9366aafcff302537b813dd854a2aaf3b2
luke0408 <zjvl324@gmail.com>
2024-03-22 19:18:05 +0900

    :recycle: Refactor: Security 설정 구현 수정
    <body>
    - requestMatcher
      - url list 사용
    - filterChain
      - value-parameter it 사용
    
    <footer>
    - 관련: #33

--- core/core-api/src/main/java/io/say/better/global/config/security/SecurityConfig.kt
+++ core/core-api/src/main/java/io/say/better/global/config/security/SecurityConfig.kt
@@ -4,6 +4,7 @@ import io.say.better.core.enums.RoleType
 import io.say.better.global.auth.handler.OAuth2LoginFailureHandler
 import io.say.better.global.auth.handler.OAuth2LoginSuccessHandler
 import io.say.better.global.auth.service.CustomOAuth2UserService
+import io.say.better.global.config.logger.logger
 import io.say.better.global.config.properties.JwtProperties
 import io.say.better.global.config.web.CorsConfig
 import io.say.better.global.jwt.filter.JwtAuthenticationProcessingFilter
@@ -14,18 +15,9 @@ import lombok.RequiredArgsConstructor
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer.UserInfoEndpointConfig
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.logout.LogoutFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
@@ -43,43 +35,56 @@ open class SecurityConfig(
     private val OAuth2LoginFailureHandler: OAuth2LoginFailureHandler
 ) {
 
+    private val permitUrls = arrayOf(
+        "/v3/api-docs/**",
+        "/swagger-ui/**",
+        "/h2-console/**",
+        "/api/temp/**",
+        "/api/auth/**",
+        "/oauth2/**"
+    )
+
+    private val noneUserRoleUrls = arrayOf(
+        "/api/auth/assign/**"
+    )
+
+    private val authenticatedUrls = arrayOf(
+        "/api/**"
+    )
+
     @Bean
     @Throws(Exception::class)
     open fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
-            .formLogin { obj: FormLoginConfigurer<HttpSecurity> -> obj.disable() }
-            .cors { corsCustomizer: CorsConfigurer<HttpSecurity?> ->
-                corsCustomizer.configurationSource(corsConfig.corsConfigurationSource)
+        http
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .cors { it.configurationSource(corsConfig.corsConfigurationSource) }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers(*permitUrls).permitAll()
+                    .requestMatchers(*noneUserRoleUrls).hasRole(RoleType.NONE.name)
+                    .requestMatchers(*authenticatedUrls).authenticated()
             }
-            .authorizeHttpRequests(Customizer<AuthorizationManagerRequestMatcherRegistry> { authorizeRequest: AuthorizationManagerRequestMatcherRegistry ->
-                authorizeRequest
-                    .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/api-docs/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher.antMatcher("/api/temp/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher.antMatcher("/api/auth/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher.antMatcher("/oauth2/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher.antMatcher("/api/auth/assign/**"))
-                    .hasRole(RoleType.NONE.name).requestMatchers(AntPathRequestMatcher.antMatcher("/api/**"))
-                    .authenticated()
-            }).oauth2Login { oauth2Login: OAuth2LoginConfigurer<HttpSecurity?> ->
-                oauth2Login.userInfoEndpoint(Customizer { userInfoEndpoint: UserInfoEndpointConfig ->
-                    userInfoEndpoint.userService(
-                        customOAuth2UserService
-                    )
-                }).successHandler(OAuth2LoginSuccessHandler).failureHandler(OAuth2LoginFailureHandler)
-            }.headers { headersConfigurer: HeadersConfigurer<HttpSecurity?> ->
-                headersConfigurer.frameOptions(Customizer<FrameOptionsConfig> { FrameOptionsConfig.sameOrigin() })
-            }.addFilterAfter(jwtAuthenticationProcessingFilter(), LogoutFilter::class.java)
+            .oauth2Login {
+                it
+                    .userInfoEndpoint { it.userService(customOAuth2UserService) }
+                    .successHandler(OAuth2LoginSuccessHandler)
+                    .failureHandler(OAuth2LoginFailureHandler)
+            }
+            .headers {
+                it
+                    .frameOptions { it.sameOrigin() }
+            }
+            .addFilterAfter(jwtAuthenticationProcessingFilter(), LogoutFilter::class.java)
         return http.build()
     }
 
     @Bean
     open fun webSecurityCustomizer(): WebSecurityCustomizer {
-        return WebSecurityCustomizer { web: WebSecurity ->
-            web.ignoring().requestMatchers(
-                PathRequest.toStaticResources().atCommonLocations()
-            )
+        return WebSecurityCustomizer {
+            it
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
         }
     }
 
