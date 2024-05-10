bb6f4c82f3281fcc65756a03791895d0cef0b6c9
asas6978 <asas6978@naver.com>
2024-05-10 09:51:35 +0900

    :sparkles: Feat - Solution 클래스 수정
    <body>
    - 클래스 파일의 pull 관련 오류가 있어 수정
    
    <footer>
    관련: #49

--- storage/storage-mysql/src/main/java/io/say/better/storage/mysql/domain/entity/Solution.java
+++ storage/storage-mysql/src/main/java/io/say/better/storage/mysql/domain/entity/Solution.java
@@ -1,16 +1,8 @@
 package io.say.better.storage.mysql.domain.entity;
 
 import io.say.better.core.enums.Status;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import io.say.better.storage.mysql.domain.constant.ProgressState;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,50 +18,45 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Solution extends BaseTimeEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "solution_id")
-	private Long solutionId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "solution_id")
+    private Long solutionId;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "writer_id", nullable = false)
-	private Educator writer;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "now_state", nullable = false)
+    private ProgressState nowState;
 
-	@Column(name = "title", nullable = false, length = 100)
-	private String title;
+    @Column(name = "educationGoal", nullable = false) // length 255
+    private String educationGoal;
 
-	@Column(name = "learner_link_cnt", nullable = false, columnDefinition = "int default 0")
-	private Integer learnerLinkCnt;
+    @Column(name = "description", nullable = false) // length 255
+    private String description;
 
-	// 기초선 회기 수
-	@Column(name = "baseline_session_time", nullable = false, columnDefinition = "int default 0")
-	private Integer baselineSessionTime;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id", nullable = false)
+    private Educator writer;
 
-	// 중재 완료하기 위한 장반응률 임계값
-	@Column(name = "inter_complete_thresh", nullable = false, columnDefinition = "double default 0.0")
-	private Double interCompleteThresh;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "learner_id")
+    private Learner learner;
 
-	// 중재 ~ 유지 사이의 기간
-	@Column(name = "inter_maintain_term", nullable = false, columnDefinition = "int default 0")
-	private Integer interMaintainTerm;
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
 
-	// 유지 회기 수
-	@Column(name = "maintain_session_time", nullable = false, columnDefinition = "int default 0")
-	private Integer maintainSessionTime;
+    // 회기당 의사소통 기회 부여 횟수
+    @Column(name = "comm_opt_times", nullable = false, columnDefinition = "int default 0")
+    private Integer commOptTimes;
 
-	// 회기당 의사소통 기회 부여 횟수
-	@Column(name = "comm_opt_times", nullable = false, columnDefinition = "int default 0")
-	private Integer commOptTimes;
+    // 의사소통 기회 부여 시간
+    @Column(name = "comm_opt_cnt", nullable = false, columnDefinition = "int default 0")
+    private Integer commOptCnt;
 
-	// 의사소통 기회 부여 시간
-	@Column(name = "comm_opt_cnt", nullable = false, columnDefinition = "int default 0")
-	private Integer commOptCnt;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private Status status;
 
-	@Enumerated(EnumType.STRING)
-	@Column(name = "status", nullable = false, length = 20)
-	private Status status;
-
-	public void onActivated() {
-		this.status = Status.ACTIVE;
-	}
+    public void onActivated() {
+        this.status = Status.ACTIVE;
+    }
 }
