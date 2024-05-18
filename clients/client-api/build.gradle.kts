c75a701f3849c99b50b44891b5754b776d04494d
luke0408 <zjvl324@gmail.com>
2024-05-18 15:33:39 +0900

    :wrench: Chore: build.gradle -> build.gradle.kts
    <footer>
    - 관련: #56 #62

---
+++ clients/client-api/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("io.github.openfeign:feign-hc5")
+    implementation("io.github.openfeign:feign-micrometer")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+}
