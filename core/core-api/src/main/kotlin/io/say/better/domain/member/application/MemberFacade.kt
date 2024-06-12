9a6014aa614d84a222d111c380cfb4604ddeec7a
luke0408 <zjvl324@gmail.com>
2024-06-13 04:09:10 +0900

    :sparkles: Feat: connect 메서드 로직 수정
    <footer>
    - 관련: #89

--- core/core-api/src/main/kotlin/io/say/better/domain/member/application/MemberFacade.kt
+++ core/core-api/src/main/kotlin/io/say/better/domain/member/application/MemberFacade.kt
@@ -1,6 +1,8 @@
 package io.say.better.domain.member.application
 
 import io.say.better.domain.member.application.impl.ConnectService
+import io.say.better.domain.member.application.impl.EducatorService
+import io.say.better.domain.member.application.impl.LearnerService
 import io.say.better.domain.member.application.impl.MemberService
 import io.say.better.domain.member.exception.MemberException
 import io.say.better.global.common.code.status.ErrorStatus
@@ -12,29 +14,31 @@ import org.springframework.stereotype.Component
 class MemberFacade(
     private val connectService: ConnectService,
     private val memberService: MemberService,
+    private val educatorService: EducatorService,
+    private val learnerService: LearnerService,
     private val codeUtil: CodeUtil,
     private val redisUtil: RedisUtil,
 ) {
     fun createConnectCode(): String {
         val member = memberService.currentMember()
         val code = codeUtil.createConnectCode()
-        redisUtil.setConnectCode(code, member.email!!)
+        redisUtil.setConnectCode(code, member.email)
 
         return code
     }
 
     fun connect(code: String) {
-        val email =
-            redisUtil.getData(code)
-                ?: throw MemberException(ErrorStatus.CONNECT_CODE_NOT_VALID)
-
-        redisUtil.deleteData(code)
+        val email = redisUtil.getData(code)
+            ?: throw MemberException(ErrorStatus.CONNECT_CODE_NOT_VALID)
 
         val educatorMember = memberService.currentMember()
         val learnerMember = memberService.getMemberByEmail(email)
+
         connectService.connect(
-            educator = memberService.getEducator(educatorMember),
-            learner = memberService.getLearner(learnerMember),
+            educator = educatorService.getEducatorByMember(educatorMember),
+            learner = learnerService.getLearnerByMember(learnerMember),
         )
+
+        redisUtil.deleteData(code)
     }
 }
