85dbc83a1f24fd2a014e71675b31fa9b659c0f06
luke0408 <zjvl324@gmail.com>
2024-06-15 17:46:50 +0900

    :wrench: Chore: 모듈명 오타 수정
    <footer>
    - 관련: #97

--- core/core-common/build.gradle.kts
+++ core/core-common/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
 
     // Jackson
-    api("com.fasterxml.jackson.core:jackson-annotation")
+    api("com.fasterxml.jackson.core:jackson-annotations")
     api("com.fasterxml.jackson.core:jackson-databind")
 }
