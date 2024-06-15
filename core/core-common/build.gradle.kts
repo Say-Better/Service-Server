4e3c902aab27a5438a974bc233761d90292b84e5
luke0408 <zjvl324@gmail.com>
2024-06-15 17:38:17 +0900

    :wrench: Chore: util class를 위한 의존성 추가
    <footer>
    - 관련: #97

--- core/core-common/build.gradle.kts
+++ core/core-common/build.gradle.kts
@@ -1,3 +1,11 @@
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Apache
     implementation("org.apache.commons:commons-lang3:3.12.0")
+
+    // Jackson
+    api("com.fasterxml.jackson.core:jackson-annotation")
+    api("com.fasterxml.jackson.core:jackson-databind")
 }
