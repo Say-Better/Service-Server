7e30521aa0efe34923cb474dccd24de36287857c
asas6978 <asas6978@naver.com>
2024-06-20 21:07:10 +0900

    :sparkles: Feat - Educator, Learner에 속성 추가
    <body>
    - Educator: 프로필 URL 추가
    - Learner: 나이, 성별, 프로필 URL 추가 및 성별 추가에 따른 Gender Enum 클래스 생성
    
    <footer>
    - 관련: #106

---
+++ storage/storage-mysql-kt/src/main/kotlin/io/say/better/storage/mysql/domain/constant/Gender.kt
@@ -0,0 +1,9 @@
+package io.say.better.storage.mysql.domain.constant
+
+enum class Gender(
+    description: String
+) {
+    MALE("남성"),
+    FEMALE("여성"),
+    ETC("기타"),
+}
--- storage/storage-mysql-kt/src/main/kotlin/io/say/better/storage/mysql/domain/entity/Learner.kt
+++ storage/storage-mysql-kt/src/main/kotlin/io/say/better/storage/mysql/domain/entity/Learner.kt
@@ -1,5 +1,6 @@
 package io.say.better.storage.mysql.domain.entity
 
+import io.say.better.storage.mysql.domain.constant.Gender
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
@@ -21,17 +22,29 @@ class Learner(
     val name: String = "",
     @Column(name = "birth_date")
     val birthDate: String = "",
+    @Column(name = "age")
+    val age: Int = 0,
+    @Column(name = "gender")
+    val gender: Gender = Gender.ETC,
+    @Column(name = "img_url")
+    val imgUrl: String = ""
 ) : BaseTimeEntity() {
     companion object {
         fun createLearner(
             memberId: Member,
             name: String = "",
             birthDate: String = "",
+            age: Int = 0,
+            gender: Gender = Gender.ETC,
+            imgUrl: String = "",
         ): Learner {
             return Learner(
                 memberId = memberId,
                 name = name,
                 birthDate = birthDate,
+                age = age,
+                gender = gender,
+                imgUrl = imgUrl,
             )
         }
     }
