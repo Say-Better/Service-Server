4aef9069b06121ab5a9c8a39d518e48f76531242
asas6978 <asas6978@naver.com>
2024-06-20 21:10:15 +0900

    :sparkles: Feat - Educator, Learner 정보 조회 api
    <footer>
    관련: #106

--- core/core-api/src/main/kotlin/io/say/better/domain/member/ui/dto/MemberResponse.kt
+++ core/core-api/src/main/kotlin/io/say/better/domain/member/ui/dto/MemberResponse.kt
@@ -1,7 +1,35 @@
 package io.say.better.domain.member.ui.dto
 
+import io.say.better.storage.mysql.domain.constant.Gender
+
 class MemberResponse private constructor() {
     init {
         throw IllegalStateException("Utility class")
+    }
+
+    data class EducatorDTO(
+        val name: String,
+        val imgUrl: String,
+    )
+
+    data class LearnerDTO(
+        val name: String,
+        val age: Int,
+        val gender: Gender,
+        val imgUrl: String,
+    )
+
+    companion object {
+        fun createEducatorDTO(
+            name: String,
+            imgUrl: String,
+        ): EducatorDTO = EducatorDTO(name, imgUrl)
+
+        fun createLearnerDTO(
+            name: String,
+            age: Int,
+            gender: Gender,
+            imgUrl: String,
+        ): LearnerDTO = LearnerDTO(name, age, gender, imgUrl)
     }
 }
