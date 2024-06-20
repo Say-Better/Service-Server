55bd5f8e9343abeda2332dbc40f07b65a38f8497
luke0408 <zjvl324@gmail.com>
2024-06-20 22:46:24 +0900

    :recycle: Refactor: 필요없는 OAuth2 파일 제거
    <footer>
    - 관련: #101

--- core/core-api/src/main/kotlin/io/say/better/global/auth/exception/AuthException.kt
+++
@@ -1,8 +0,0 @@
-package io.say.better.global.auth.exception
-
-import io.say.better.core.common.code.BaseErrorCode
-import io.say.better.core.common.exception.GeneralException
-
-class AuthException(
-    code: BaseErrorCode,
-) : GeneralException(code)
