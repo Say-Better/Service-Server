81f9d747aba4ff4561035fcf3d31cb7e8566b821
asas6978 <asas6978@naver.com>
2024-06-23 13:17:19 +0900

    :bug: Bug - ReactionType 클래스 속성 값 오탈자 수정
    <footer>
    - 관련: #110

--- storage/storage-mysql-kt/src/main/kotlin/io/say/better/storage/mysql/domain/constant/ReactionType.kt
+++ storage/storage-mysql-kt/src/main/kotlin/io/say/better/storage/mysql/domain/constant/ReactionType.kt
@@ -1,7 +1,9 @@
 package io.say.better.storage.mysql.domain.constant
 
-enum class ReactionType(description: String) {
-    TURE("정반응"),
+enum class ReactionType(
+    description: String,
+) {
+    TRUE("정반응"),
     FALSE("오반응"),
     NONE("없음"),
 }
