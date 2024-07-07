f1e93c5b58b7c58edb8751a9abf130d1dfb8b25e
luke0408 <zjvl324@gmail.com>
2024-07-08 04:52:25 +0900

    :sparkles: Feat: 학습자 교육자 Fixtures 정의
    <footer>
    - 관련: #115

---
+++ core/core-api/src/test/kotlin/io/say/better/domain/member/EducatorFixtures.kt
@@ -0,0 +1,21 @@
+package io.say.better.domain.member
+
+import io.say.better.storage.mysql.domain.entity.Educator
+import io.say.better.storage.mysql.domain.entity.Member
+
+const val EDUCATOR_NAME: String = "educator_name"
+const val EDUCATOR_BIRTH_DAY: String = "1998-01-01"
+const val EDUCATOR_PROFILE_URL: String = "educator_profile_url"
+
+fun createEducator(
+    member: Member,
+    name: String = EDUCATOR_NAME,
+    birthDay: String = EDUCATOR_BIRTH_DAY,
+    profileUrl: String = EDUCATOR_PROFILE_URL,
+): Educator =
+    Educator.createEducator(
+        memberId = member,
+        name = name,
+        birthDate = birthDay,
+        imgUrl = profileUrl,
+    )
