6fa069006fe5aff1c8cced83bd9c38f625edd70c
luke0408 <zjvl324@gmail.com>
2024-07-07 17:08:33 +0900

    :test_tube: Test: 이메일 암호화 복호화 테스트
    <footer>
    - 관련: #115

---
+++ core/core-api/src/test/kotlin/io/say/better/domain/member/application/helper/EmailEncoderHelperTest.kt
@@ -0,0 +1,52 @@
+package io.say.better.domain.member.application.helper
+
+import io.say.better.DevelopTest
+import io.say.better.global.utils.AesEncryptUtil
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor
+
+class EmailEncoderHelperTest : DevelopTest() {
+    private lateinit var aesBytesEncryptor: AesBytesEncryptor
+    private lateinit var aesEncryptUtil: AesEncryptUtil
+    private lateinit var emailEncoderHelper: EmailEncoderHelper
+
+    @BeforeEach
+    fun setUp() {
+        aesBytesEncryptor = AesBytesEncryptor("password", "4b0febebd439db7ca77153cb254520c3")
+        aesEncryptUtil = AesEncryptUtil(aesBytesEncryptor)
+        emailEncoderHelper = EmailEncoderHelper(aesBytesEncryptor, aesEncryptUtil)
+    }
+
+    @Test
+    @DisplayName("Email을 AES 알고리즘으로 암호화한다.")
+    fun encoder() {
+        // given
+        val email = "test@gmail.com"
+
+        // when
+        val encoded = emailEncoderHelper.encoder(email)
+
+        // that
+        assertThat(encoded)
+            .isNotNull()
+            .isNotEmpty()
+            .isNotEqualTo(email)
+    }
+
+    @Test
+    @DisplayName("AES 알고리즘으로 암호화된 Email을 복호화한다.")
+    fun decoder() {
+        // given
+        val email = "test@gmail.com"
+        val encoded = emailEncoderHelper.encoder(email)
+
+        // when
+        val decoded = emailEncoderHelper.decoder(encoded)
+
+        // that
+        assertThat(decoded).isEqualTo(email)
+    }
+}
