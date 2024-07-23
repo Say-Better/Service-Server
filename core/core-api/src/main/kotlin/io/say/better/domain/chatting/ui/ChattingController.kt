a16b1270a2cf6599f0a7387e69792147e8b188c4
luke0408 <zjvl324@gmail.com>
2024-07-23 23:09:09 +0900

    :recycle: Refactor: 필요없어진 어노테이션 및 기능 삭제
    <footer>
    - 관련: #125

--- core/core-api/src/main/kotlin/io/say/better/domain/chatting/ui/ChattingController.kt
+++ core/core-api/src/main/kotlin/io/say/better/domain/chatting/ui/ChattingController.kt
@@ -4,13 +4,11 @@ import io.say.better.core.common.response.ResponseDto
 import io.say.better.domain.chatting.application.ChattingFacade
 import io.say.better.domain.chatting.ui.dto.ChattingRequest
 import io.say.better.domain.chatting.ui.dto.ChattingResponse
-import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = "Chatting", description = "Chatting API")
 @RestController
 @RequestMapping("/api/chatting")
 class ChattingController(
