e636ac6e673e4cd6430f470de5bd1cea9757dada
asas6978 <asas6978@naver.com>
2024-08-11 21:01:49 +0900

    :bug: Bug - 채팅 API 메서드 수정
    <body>
    - 채팅 api 메서드 GET -> POST로 수정
    
    <footer>
    - 관련: #144

--- core/core-api/src/main/kotlin/io/say/better/domain/chatting/ui/ChattingController.kt
+++ core/core-api/src/main/kotlin/io/say/better/domain/chatting/ui/ChattingController.kt
@@ -4,7 +4,7 @@ import io.say.better.core.common.response.ResponseDto
 import io.say.better.domain.chatting.application.ChattingFacade
 import io.say.better.domain.chatting.ui.dto.ChattingRequest
 import io.say.better.domain.chatting.ui.dto.ChattingResponse
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 class ChattingController(
     private val chattingFacade: ChattingFacade,
 ) {
-    @GetMapping("")
+    @PostMapping("")
     fun chat(
         @RequestBody request: ChattingRequest,
     ): ResponseDto<ChattingResponse> {
