fe144b895abc86f2b7330b1de5fa9c53e1d004c1
destiny3912 <destiny3910@gmail.com>
2024-09-01 16:38:32 +0900

    :sparkles: Feat: 리뷰 제출 API

--- core/core-api/src/main/kotlin/io/say/better/domain/review/ui/ReviewController.kt
+++ core/core-api/src/main/kotlin/io/say/better/domain/review/ui/ReviewController.kt
@@ -3,7 +3,10 @@ package io.say.better.domain.review.ui
 import io.say.better.core.common.response.ResponseDto
 import io.say.better.domain.review.application.ReviewFacade
 import io.say.better.domain.review.ui.dto.ReviewRequest
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/review")
